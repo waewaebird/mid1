b3d9561f7e14ead5c65333bf711f77c03816792a
jzangeva <jzangeva@auto-focus.co.kr>
2024-12-06 15:49:17 +0900

    first

---
+++ src/lang/string/StringBasicMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class StringBasicMain {
+    public static void main(String[] args) {
+        //클래스 참조형(속성과 기능을 부여 받음)
+        String str1 = "hello";
+        String str2 = new String("hello");
+
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
