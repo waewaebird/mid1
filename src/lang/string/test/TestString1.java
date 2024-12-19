38319184d5e26912ac4d416bbea825210628e5fa
jzangeva <jzangeva@auto-focus.co.kr>
2024-12-19 16:34:12 +0900

    ~ String 클래스 정리

--- src/lang/string/test/TestString1.java
+++ src/lang/string/test/TestString1.java
@@ -1,4 +1,12 @@
 package lang.string.test;
 
 public class TestString1 {
+
+    public static void main(String[] args) {
+        String url = "https://www.example.com";
+
+        System.out.println("result : " + url.startsWith("https://"));
+
+    }
+
 }
